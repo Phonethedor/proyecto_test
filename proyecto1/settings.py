f0eeeb1db9da29a39b6462a2171e83a7be41b4d8
Phonethedor <alvaroc.giovannim@gmail.com>
2021-08-21 10:34:11 -0400

    1

--- proyecto1/settings.py
+++ proyecto1/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'proyecto1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['clientes/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
